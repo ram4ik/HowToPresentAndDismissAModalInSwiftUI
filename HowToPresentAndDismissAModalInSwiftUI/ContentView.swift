ffcf3b30a9c21fdd291f8868962b110b309baa56
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 11:58:07 +0300

    init

--- HowToPresentAndDismissAModalInSwiftUI/ContentView.swift
+++ HowToPresentAndDismissAModalInSwiftUI/ContentView.swift
@@ -8,9 +8,35 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var body: some View {
+        Button(action: {}) {
+            Text("Dismiss")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(15)
+                .shadow(radius: 15)
+            
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                NavigationLink(destination: DetailView()) {
+                    Text("Present")
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(15)
+                        .shadow(radius: 15)
+                    
+                }
+            }
+        }
     }
 }
 
